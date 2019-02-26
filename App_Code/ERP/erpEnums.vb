cda0e843d1c60c6c1e2f8a84b9ad09b0f8299caa
0340LalitGupta <lalit@isgec.co.in>
2019-02-26 14:10:48 +0530

    Transporter Bill Functionality

---
+++ App_Code/ERP/erpEnums.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Enum TptBillStatus
+	Cancelled = 1
+	Free = 2
+	UnderReceiveByAccounts = 3
+	UnderReceiveByLogistics = 4
+	UnderReSubmitbyLogistics = 5
+	UnderPaymentProcessing = 6
+	PaymentProcessed = 7
+	Closed = 8
+	PTRCreated = 9
+End Enum
+
